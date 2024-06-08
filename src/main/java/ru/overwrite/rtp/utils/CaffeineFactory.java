ca4a6c0ef99d9752eb0be3a93af137ef0220c8f8
Overwrite987 <artem.korolev_2003@mail.ru>
2024-06-08 04:46:08 +0300

    mark original author

--- src/main/java/ru/overwrite/rtp/utils/CaffeineFactory.java
+++ src/main/java/ru/overwrite/rtp/utils/CaffeineFactory.java
@@ -1,3 +1,5 @@
+// Original - https://github.com/LuckPerms/LuckPerms/blob/master/common/src/main/java/me/lucko/luckperms/common/util/CaffeineFactory.java
+
 package ru.overwrite.rtp.utils;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -8,17 +10,6 @@ import java.util.concurrent.ForkJoinPool;
 public final class CaffeineFactory {
     private CaffeineFactory() {}
 
-    /**
-     * Our own fork join pool for LuckPerms cache operations.
-     *
-     * By default, Caffeine uses the ForkJoinPool.commonPool instance.
-     * However... ForkJoinPool is a fixed size pool limited by Runtime.availableProcessors.
-     * Some (bad) plugins incorrectly use this pool for i/o operations, make calls to Thread.sleep
-     * or otherwise block waiting for something else to complete. This prevents the LP cache loading
-     * operations from running.
-     *
-     * By using our own pool, we ensure this will never happen.
-     */
     private static final ForkJoinPool loaderPool = new ForkJoinPool();
 
     public static Caffeine<Object, Object> newBuilder() {
