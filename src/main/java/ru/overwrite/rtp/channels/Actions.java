debd6496c35695ce26496a45504458d6aeee3b56
Daniil Zao. <idanix@list.ru>
2024-08-15 22:55:21 +0300

    Replace primitive Map with fastutil alternative

--- src/main/java/ru/overwrite/rtp/channels/Actions.java
+++ src/main/java/ru/overwrite/rtp/channels/Actions.java
@@ -1,12 +1,12 @@
 package ru.overwrite.rtp.channels;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import ru.overwrite.rtp.actions.Action;
 
 import java.util.List;
-import java.util.Map;
 
 public record Actions(
         List<Action> preTeleportActions,
-        Map<Integer, List<Action>> onCooldownActions,
+        Int2ObjectMap<List<Action>> onCooldownActions,
         List<Action> afterTeleportActions) {
 }
