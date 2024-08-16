1e0554980acd2073dc3f86c98bcb64e913c59fb6
Overwrite987 <artem.korolev_2003@mail.ru>
2024-08-16 18:53:57 +0300

    remove unused imports

--- src/main/java/ru/overwrite/rtp/utils/Utils.java
+++ src/main/java/ru/overwrite/rtp/utils/Utils.java
@@ -14,9 +14,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Locale;
 import java.util.function.Consumer;
-import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
