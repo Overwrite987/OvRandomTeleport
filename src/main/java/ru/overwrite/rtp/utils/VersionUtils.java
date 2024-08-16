25b308ef63052c590aad992fcc55079bbe7b2cd0
Overwrite987 <artem.korolev_2003@mail.ru>
2024-08-16 15:24:02 +0300

    simplify VersionUtils

--- src/main/java/ru/overwrite/rtp/utils/VersionUtils.java
+++ src/main/java/ru/overwrite/rtp/utils/VersionUtils.java
@@ -3,9 +3,9 @@ package ru.overwrite.rtp.utils;
 import org.bukkit.Bukkit;
 
 public final class VersionUtils {
-    private VersionUtils() { }
 
     public static final int SUB_VERSION = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
 
     public static final int VOID_LEVEL = SUB_VERSION >= 18 ? -60 : 0;
+
 }
