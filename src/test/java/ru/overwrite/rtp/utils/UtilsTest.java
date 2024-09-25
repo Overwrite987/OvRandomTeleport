77a0bede1757a87a207a9e33f8938a2e7246b5f0
Overwrite987 <artem.korolev_2003@mail.ru>
2024-09-25 03:58:13 +0300

    add test for replaceEach

--- src/test/java/ru/overwrite/rtp/utils/UtilsTest.java
+++ src/test/java/ru/overwrite/rtp/utils/UtilsTest.java
@@ -1,6 +1,7 @@
 package ru.overwrite.rtp.utils;
 
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -10,7 +11,7 @@ public class UtilsTest {
 
     @DataProvider
     public Object[][] translateAlternateColorCodesData() {
-        return new Object[][] {
+        return new Object[][]{
                 {"Test"},
                 {"&aHello world"},
                 {"&r"},
@@ -24,5 +25,38 @@ public class UtilsTest {
                 Utils.translateAlternateColorCodes('&', input),
                 ChatColor.translateAlternateColorCodes('&', input)
         );
+    }
+
+    @DataProvider
+    public Object[][] replaceEachData() {
+        return new Object[][]{
+                {"Test", new String[]{"Test"}, new String[]{"Tset"}},
+                {"Hello World", new String[]{"Hello"}, new String[]{"Bye"}},
+                {"CaSe TeSt", new String[]{"CaSe", "tEsT"}, new String[]{"cAsE", "FaIl"}}
+        };
+    }
+
+    @Test(dataProvider = "replaceEachData")
+    public void replaceEachTest(String input, String[] searchList, String[] replacementList) {
+        assertEquals(
+                Utils.replaceEach(input, searchList, replacementList),
+                replaceNormal(input, searchList, replacementList)
+        );
+    }
+
+    private static String replaceNormal(@NotNull String text, @NotNull String[] searchList, @NotNull String[] replacementList) {
+        if (text.isEmpty() || searchList.length == 0 || replacementList.length == 0) {
+            return text;
+        }
+
+        if (searchList.length != replacementList.length) {
+            throw new IllegalArgumentException("Search and replacement arrays must have the same length.");
+        }
+
+        for (int i = 0; i < searchList.length; i++) {
+            text = text.replace(searchList[i], replacementList[i]);
+        }
+
+        return text;
     }
 }
