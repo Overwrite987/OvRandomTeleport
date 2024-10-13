73a41afa1de0ffb4b7b0f1479387b8d68db83bf0
Overwrite987 <artem.korolev_2003@mail.ru>
2024-10-13 05:23:10 +0300

    make searchList private

--- src/main/java/ru/overwrite/rtp/RtpManager.java
+++ src/main/java/ru/overwrite/rtp/RtpManager.java
@@ -431,7 +431,7 @@ public class RtpManager {
         return groupCooldowns.getOrDefault(playerGroup, defaultCooldown);
     }
 
-    final String[] searchList = {"%player%", "%name%", "%time%", "%x%", "%y%", "%z%"};
+    private final String[] searchList = {"%player%", "%name%", "%time%", "%x%", "%y%", "%z%"};
 
     public void executeActions(Player p, Channel channel, List<Action> actions, Location loc) {
         if (actions.isEmpty()) {
