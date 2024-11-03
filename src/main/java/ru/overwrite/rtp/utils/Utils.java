8c6aec5a0ef07c9850f374de94175c01b0db3c38
Overwrite987 <artem.korolev_2003@mail.ru>
2024-11-03 08:48:09 +0300

    improve colorizer even more

--- src/main/java/ru/overwrite/rtp/utils/Utils.java
+++ src/main/java/ru/overwrite/rtp/utils/Utils.java
@@ -1,7 +1,5 @@
 package ru.overwrite.rtp.utils;
 
-import it.unimi.dsi.fastutil.chars.CharOpenHashSet;
-import it.unimi.dsi.fastutil.chars.CharSet;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -54,25 +52,29 @@ public class Utils {
         });
     }
 
-    private static final CharSet CODES = new CharOpenHashSet(new char[]{
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'a', 'b', 'c', 'd', 'e', 'f',
-            'A', 'B', 'C', 'D', 'E', 'F',
-            'k', 'l', 'm', 'n', 'o', 'r', 'x',
-            'K', 'L', 'M', 'N', 'O', 'R', 'X'
-    });
-
     public static String translateAlternateColorCodes(char altColorChar, String textToTranslate) {
         char[] b = textToTranslate.toCharArray();
 
         for (int i = 0, length = b.length - 1; i < length; ++i) {
-            if (b[i] == altColorChar && CODES.contains(b[i + 1])) {
+            if (b[i] == altColorChar && isValidColorCharacter(b[i + 1])) {
                 b[i++] = '§';
                 b[i] = Character.toLowerCase(b[i]);
             }
         }
 
         return new String(b);
+    }
+
+    private static boolean isValidColorCharacter(char c) {
+        return (c >= '0' && c <= '9') ||
+                (c >= 'a' && c <= 'f') ||
+                c == 'r' ||
+                (c >= 'k' && c <= 'o') ||
+                c == 'x' ||
+                (c >= 'A' && c <= 'F') ||
+                c == 'R' ||
+                (c >= 'K' && c <= 'O') ||
+                c == 'X';
     }
 
     public static boolean USE_PAPI = false;
