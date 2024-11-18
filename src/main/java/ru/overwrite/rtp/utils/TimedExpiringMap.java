60ef2691b675648cac01117470fa5f6f44e97891
Overwrite987 <artem.korolev_2003@mail.ru>
2024-11-18 03:30:01 +0300

    Update TimedExpiringMap.java

--- src/main/java/ru/overwrite/rtp/utils/TimedExpiringMap.java
+++ src/main/java/ru/overwrite/rtp/utils/TimedExpiringMap.java
@@ -12,24 +12,22 @@ public class TimedExpiringMap<K, V> {
 
     public TimedExpiringMap(TimeUnit unit) {
         this.unit = unit;
-        this.cache = CaffeineFactory.newBuilder()
-                .expireAfter(new Expiry<K, ExpiringValue<V>>() {
-                    @Override
-                    public long expireAfterCreate(K key, ExpiringValue<V> value, long currentTime) {
-                        return value.expiryDuration();
-                    }
+        this.cache = CaffeineFactory.newBuilder().expireAfter(new Expiry<K, ExpiringValue<V>>() {
+            @Override
+            public long expireAfterCreate(K key, ExpiringValue<V> value, long currentTime) {
+                return value.expiryDuration();
+            }
 
-                    @Override
-                    public long expireAfterUpdate(K key, ExpiringValue<V> value, long currentTime, long currentDuration) {
-                        return value.expiryDuration();
-                    }
+            @Override
+            public long expireAfterUpdate(K key, ExpiringValue<V> value, long currentTime, long currentDuration) {
+                return value.expiryDuration();
+            }
 
-                    @Override
-                    public long expireAfterRead(K key, ExpiringValue<V> value, long currentTime, long currentDuration) {
-                        return currentDuration;
-                    }
-                })
-                .build();
+            @Override
+            public long expireAfterRead(K key, ExpiringValue<V> value, long currentTime, long currentDuration) {
+                return currentDuration;
+            }
+        }).build();
     }
 
     public void put(K key, V value, long duration) {
