dead9b2f2cf8b007eac2e00b2c80318a57e4e953
Overwrite987 <artem.korolev_2003@mail.ru>
2024-12-28 07:27:27 +0300

    use correct void level on new versions

--- src/main/java/ru/overwrite/rtp/utils/VersionUtils.java
+++ src/main/java/ru/overwrite/rtp/utils/VersionUtils.java
@@ -8,6 +8,6 @@ public final class VersionUtils {
 
     public static final int SUB_VERSION = Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
 
-    public static final int VOID_LEVEL = SUB_VERSION >= 18 ? -60 : 0;
+    public static final int VOID_LEVEL = SUB_VERSION >= 18 ? -64 : 0;
 
 }
