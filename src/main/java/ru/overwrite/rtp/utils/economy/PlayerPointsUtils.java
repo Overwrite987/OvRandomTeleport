54d92ede271bdb2c3225573caf7d8f609fac4b18
Overwrite987 <artem.korolev_2003@mail.ru>
2025-01-03 09:59:38 +0300

    fuck

--- src/main/java/ru/overwrite/rtp/utils/economy/PlayerPointsUtils.java
+++ src/main/java/ru/overwrite/rtp/utils/economy/PlayerPointsUtils.java
@@ -13,16 +13,16 @@ public final class PlayerPointsUtils {
     private static final PlayerPointsAPI API = PlayerPoints.getInstance().getAPI();
 
     public static void withdraw(Player player, int amount) {
-        UUID uuid = player.getName();
+        UUID uuid = player.getUniqueId();
         API.take(uuid, amount);
     }
 
     public static void deposit(Player player, int amount) {
-        UUID uuid = player.getName();
+        UUID uuid = player.getUniqueId();
         API.give(uuid, amount);
     }
 
     public static int getBalance(Player player) {
-        return API.look(player.getName());
+        return API.look(player.getUniqueId());
     }
 }
