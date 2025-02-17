82f97ef32e8013bc642c2ba82e2d7460cf15155e
Overwrite987 <artem.korolev_2003@mail.ru>
2025-02-17 13:34:25 +0300

    remove unused import

--- src/main/java/ru/overwrite/rtp/RtpCommand.java
+++ src/main/java/ru/overwrite/rtp/RtpCommand.java
@@ -23,7 +23,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class RtpCommand implements CommandExecutor, TabCompleter {
 
