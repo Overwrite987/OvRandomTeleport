dd0fc419a8f3c1fc87b2753aaeb2d2213d3d68c2
Jodexx <0984134q@gmail.com>
2025-10-07 20:09:03 +0200

    Improve constants naming, enhance dot calculation, and fix spawning logic

--- src/main/java/ru/overwrite/rtp/animations/impl/CageAnimation.java
+++ src/main/java/ru/overwrite/rtp/animations/impl/CageAnimation.java
@@ -17,20 +17,22 @@ public class CageAnimation extends Animation {
     private final double RADIUS;
 
     // must have more settings in the config
-    private static final int DOTS = 32; // DOTS | LINES
-    private static final int LINES = 16; // <= DOTS
-    private static final int COUNT_PER_LINE = 8;
+    private static final int MAX_DOTS = 32; // DOTS | LINES
+    private static final int MAX_LINES = 16; // <= DOTS
 
-    private static final double OFFSET = C / DOTS;
+    private static final int DOTS_PER_LINE = Math.max(MAX_DOTS, MAX_LINES) / Math.min(MAX_DOTS, MAX_LINES); // fix for DOTS | LINES
+    private static final int COUNT_PER_LINE = 8; // how much dots inside the line
 
-    private static final List<Double> CIRCLES = List.of(2.0, 0.0);
+    private static final double CIRCLE_OFFSET = C / MAX_DOTS;
+
+    private static final List<Double> CIRCLES = List.of(2.0, 1.0, 0.0); // circle y offsets (do not use the same values)
 
     private final double first = CIRCLES.getFirst();
     private final double last = CIRCLES.getLast();
 
     private final double LINE_OFFSET = (first - last) / COUNT_PER_LINE;
 
-    final ParticleBuilder builder = Particle.DUST.builder()
+    private final ParticleBuilder builder = Particle.DUST.builder()
             .particle()
             .builder()
             .count(1)
@@ -40,7 +42,9 @@ public class CageAnimation extends Animation {
             .receivers(receivers)
             .source(player);
 
-    private final Location location = player.getLocation();
+    private final int DOT_PER_TICKS = (int) Math.floor((double) Math.max((duration), MAX_DOTS) / Math.min(duration, MAX_DOTS));
+
+    private int currentDots = 0;
 
     public CageAnimation(Player player, int duration, Particles particles) {
         super(player, duration, particles);
@@ -56,26 +60,32 @@ public class CageAnimation extends Animation {
             return;
         }
 
-        double angle = 0;
+        final Location location = player.getLocation();
+
+        if (tickCounter % DOT_PER_TICKS == 0 && currentDots < MAX_DOTS) {
+            currentDots++;
+        }
 
         for (int circle = 0; circle < CIRCLES.size(); circle++) {
+            double angle = 0;
+
             Double yOffset = CIRCLES.get(circle);
-            for (int i = 0; i < DOTS; i++) {
+            for (int i = 0; i < currentDots; i++) {
                 double x = Math.cos(angle) * RADIUS;
                 double z = Math.sin(angle) * RADIUS;
 
-                builder.clone().location(this.location.clone().add(x, yOffset, z)).spawn();
+                builder.clone().location(location.clone().add(x, yOffset, z)).spawn();
 
                 // only one time per circle
                 if (circle == 0) {
-                    if (i % (DOTS / LINES) == 0) {
+                    if (i % (DOTS_PER_LINE) == 0) {
                         for (double y = last; y <= first; y += LINE_OFFSET) {
-                            builder.clone().location(this.location.clone().add(x, y, z)).spawn();
+                            builder.clone().data(new Particle.DustOptions(Color.RED, 0.5f)).location(location.clone().add(x, y, z)).spawn();
                         }
                     }
                 }
 
-                angle += OFFSET;
+                angle += CIRCLE_OFFSET;
 
             }
         }
